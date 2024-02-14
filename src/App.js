c5b9fafb15356762fa4071126448b4416877870c
thomastam456 <tana6c21@gmail.com>
2024-02-14 13:25:23 +0800

    Update HashRouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import './styles/style.scss';
 import Home from './pages/home'; 
 import Contact from './pages/contact';
@@ -6,13 +6,13 @@ import Contact from './pages/contact';
 export default function App() {
    return (
       <div>
-         <BrowserRouter>
+         <HashRouter>
             <Routes>
                {/* Set Routes of pages */}
                <Route index element={<Home />} />
                <Route path="/contact" element={<Contact />} />
             </Routes>
-         </BrowserRouter>
+         </HashRouter>
       </div>
    )
 }
